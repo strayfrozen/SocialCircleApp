8926c7ab8c9725886ddb5f51a413496ad6005c7a
Stray <strayfrozen@yahoo.com>
2022-02-11 11:58:46 -0600

    created reactionSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,34 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const reactionSchema = new Schema(
+  {
+     reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      //280 character maximum
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
 const thoughtSchema = new Schema(
     {
         thoughtText: {

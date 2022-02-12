748db6e4890518e4d34e8931e8ad19728fa98b7e
Stray <strayfrozen@yahoo.com>
2022-02-12 11:08:42 -0600

    created the post update and delete controller in user-controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,26 +1,59 @@
-const {User,Thought} = require("../models")
+const { User, Thought } = require("../models")
 
-const userController ={
+const userController = {
 
-getUsers(req,res){
-    User.find()
-    .then((userData)=>{
-        res.json(userData)
-    })
-},
+    getUsers(req, res) {
+        User.find()
+            .then((userData) => {
+                res.json(userData)
+            })
+    },
 
-getSingleUser(req,res){
-    User.findOne({
-        _id: req.params.userId,
-        
+    getSingleUser(req, res) {
+        User.findOne({
+            _id: req.params.userId,
+        })
+            .populate('friends')
+            .populate('thoughts')
+            .then((userData) => {
+                res.json(userData)
+            })
+    },
 
-    })
-    .populate('friends')
-    .populate('thoughts')
-    .then((userData)=>{
-        res.json(userData)
-    })
-}
+
+    // create User
+    createUser({ body }, res) {
+        User.create(body)
+            .then(userData => res.json(userData))
+            .catch(err => res.status(400).json(err));
+    },
+
+
+    // update User
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(userData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+     // delete pizza
+     deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(userData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
 
 
 }

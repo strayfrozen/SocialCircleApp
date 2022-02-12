3e78203797936d0b7547fce61b19a2235ef62851
Stray <strayfrozen@yahoo.com>
2022-02-12 11:18:57 -0600

    updated index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
 router.use('/users',userRoutes)
-//this is where my thoughts-route will go
+router.use('/thoughts',thoughtRoutes)
+
 
 
 
